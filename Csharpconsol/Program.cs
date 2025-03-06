57a7e246eb2bcbdef3b2df082db84382ea1fb326
Ilkin Bayram <ilkinjb-ap120@code.edu.az>
2025-03-06 09:52:13 +0400

    Add project files.

---
+++ Csharpconsol/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Birinci ededi daxil edin:");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ikinci ededi daxil edin:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (x % 2 == 0 && n % 2 == 0)
+        {
+            int cem = x + n;
+            Console.WriteLine("Cem: " + cem);
+        }
+        else
+        {
+            Console.WriteLine("Daxil olan ededler cut olmalidir");
+        }
+
+       
+
+    }
+}
